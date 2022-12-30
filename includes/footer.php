020ae4e32c0f34a8db756f5737fabe0d01677c21
Satissh <82075459+satisshTechie@users.noreply.github.com>
2022-12-31 04:06:12 +0530

    Last Changes

---
+++ includes/footer.php
@@ -0,0 +1,170 @@
+ <!-- Footer -->
+ <footer
+        id="footer"
+        class="footer"
+        data-bg-img="images/footer-bg.png"
+        data-bg-color="#262E3B"
+      >
+        <div class="container pt-70 pb-40">
+          <div class="row border-bottom-black">
+            <div class="col-sm-6 col-md-3">
+              <div class="widget dark">
+                <img alt="" src="images/dheera/dheera-logo.png" />
+                <p class="font-13 mt-20 mb-10">
+                The Dheera Foundation was established in 2021 with an intention for betterment & uplift of society. 
+                  <a class="text-theme-colored" href="#">read more</a>
+                </p>
+              </div>
+            </div>
+            <div class="col-sm-6 col-md-3">
+              <div class="widget dark">
+                <h5 class="widget-title line-bottom">Latest News</h5>
+                <div class="latest-posts">
+                  <article class="post media-post clearfix pb-0 mb-10">
+                    <a href="#" class="post-thumb"
+                      ><img alt="" src="http://placehold.it/80x55"
+                    /></a>
+                    <div class="post-right">
+                      <h5 class="post-title mt-0 mb-5">
+                        <a href="#">Education</a>
+                      </h5>
+                      <p class="post-date mb-0 font-12">Mar 08, 2022</p>
+                    </div>
+                  </article>
+                  <article class="post media-post clearfix pb-0 mb-10">
+                    <a href="#" class="post-thumb"
+                      ><img alt="" src="http://placehold.it/80x55"
+                    /></a>
+                    <div class="post-right">
+                      <h5 class="post-title mt-0 mb-5">
+                        <a href="#">Health Care</a>
+                      </h5>
+                      <p class="post-date mb-0 font-12">Mar 08, 2022</p>
+                    </div>
+                  </article>
+                  <article class="post media-post clearfix pb-0 mb-10">
+                    <a href="#" class="post-thumb"
+                      ><img alt="" src="http://placehold.it/80x55"
+                    /></a>
+                    <div class="post-right">
+                      <h5 class="post-title mt-0 mb-5">
+                        <a href="#">Women Empowerment</a>
+                      </h5>
+                      <p class="post-date mb-0 font-12">Mar 08, 2022</p>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-6 col-md-3">
+              <div class="widget dark">
+                <h5 class="widget-title line-bottom">Call Us Now</h5>
+                <div id="flickr-feed" class="clearfix">
+                  <div class="text-gray">
+                    +91 9666886848 <br />
+                    +91 9912551287
+                  </div>
+                  <script type="text/javascript" src=""></script>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm-6 col-md-3">
+              <div class="widget dark">
+                <h5 class="widget-title line-bottom">Connect With Us</h5>
+
+                <ul
+                  class="styled-icons icon-sm icon-bordered icon-circled clearfix"
+                >
+                  <li>
+                    <a href="#"><i class="fa fa-facebook"></i></a>
+                  </li>
+                  <li>
+                    <a href="#"><i class="fa fa-twitter"></i></a>
+                  </li>
+                  <li>
+                    <a href="#"><i class="fa fa-instagram"></i></a>
+                  </li>
+
+                  <li>
+                    <a href="#"><i class="fa fa-instagram"></i></a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="footer-bottom" data-bg-color="#242730">
+          <div class="container pt-15 pb-15">
+            <div class="row">
+              <div class="col-md-6">
+                <p class="font-12 text-black-777 m-0 sm-text-center">
+                  Copyright &copy; 2023 DHEERA Foundation. All Rights Reserved
+                </p>
+              </div>
+              <div class="col-md-6 text-right">
+                <div class="widget no-border m-0">
+                  <ul class="list-inline sm-text-center font-12">
+                    <li>
+                      <a href="#">FAQ</a>
+                    </li>
+                    <li>|</li>
+                    <li>
+                      <a href="#">Privacy Policy</a>
+                    </li>
+                    <li>|</li>
+                    <li>
+                      <a href="#">Terms Of Service</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <a class="scrollToTop" href="#"><i class="fa fa-angle-up"></i></a>
+    </div>
+    <!-- end wrapper -->
+
+    <!-- Footer Scripts -->
+    <!-- JS | Custom script for all pages -->
+    <script src="js/custom.js"></script>
+
+    <script
+      type="text/javascript"
+      src="js/revolution-slider/js/extensions/revolution.extension.actions.min.js"
+    ></script>
+    <script
+      type="text/javascript"
+      src="js/revolution-slider/js/extensions/revolution.extension.carousel.min.js"
+    ></script>
+    <script
+      type="text/javascript"
+      src="js/revolution-slider/js/extensions/revolution.extension.kenburn.min.js"
+    ></script>
+    <script
+      type="text/javascript"
+      src="js/revolution-slider/js/extensions/revolution.extension.layeranimation.min.js"
+    ></script>
+    <script
+      type="text/javascript"
+      src="js/revolution-slider/js/extensions/revolution.extension.migration.min.js"
+    ></script>
+    <script
+      type="text/javascript"
+      src="js/revolution-slider/js/extensions/revolution.extension.navigation.min.js"
+    ></script>
+    <script
+      type="text/javascript"
+      src="js/revolution-slider/js/extensions/revolution.extension.parallax.min.js"
+    ></script>
+    <script
+      type="text/javascript"
+      src="js/revolution-slider/js/extensions/revolution.extension.slideanims.min.js"
+    ></script>
+    <script
+      type="text/javascript"
+      src="js/revolution-slider/js/extensions/revolution.extension.video.min.js"
+    ></script>
+  </body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+  <head>
+    <!-- Meta Tags -->
+    <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+    <meta
+      name="description"
+      content="DHEERA Foundation - Nonprofit Charity Organization"
+    />
+
+    <!-- Page Title -->
+    <title>DHEERA Foundation - Dr. Botcha Sandeep</title>
+
+    <!-- Favicon and Touch Icons -->
+    <link
+      href="images/dheera/favicon.png"
+      rel="shortcut icon"
+      type="image/png"
+    />
+    <link href="images/dheera/favicon.png" rel="apple-touch-icon" />
+    <link
+      href="images/dheera/favicon.png"
+      rel="apple-touch-icon"
+      sizes="72x72"
+    />
+    <link
+      href="images/dheera/favicon.png"
+      rel="apple-touch-icon"
+      sizes="114x114"
+    />
+    <link
+      href="images/dheera/favicon.png"
+      rel="apple-touch-icon"
+      sizes="144x144"
+    />
+
+    <!-- Stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="css/jquery-ui.min.css" rel="stylesheet" type="text/css" />
+    <link href="css/animate.css" rel="stylesheet" type="text/css" />
+    <link href="css/css-plugin-collections.css" rel="stylesheet" />
+    <!-- CSS | menuzord megamenu skins -->
+    <link
+      id="menuzord-menu-skins"
+      href="css/menuzord-skins/menuzord-rounded-boxed.css"
+      rel="stylesheet"
+    />
+    <!-- CSS | Main style file -->
+    <link href="css/style-main.css" rel="stylesheet" type="text/css" />
+    <!-- CSS | Preloader Styles -->
+    <link href="css/preloader.css" rel="stylesheet" type="text/css" />
+    <!-- CSS | Custom Margin Padding Collection -->
+    <link
+      href="css/custom-bootstrap-margin-padding.css"
+      rel="stylesheet"
+      type="text/css"
+    />
+    <!-- CSS | Responsive media queries -->
+    <link href="css/responsive.css" rel="stylesheet" type="text/css" />
+    <!-- CSS | Style css. This is the file where you can place your own custom css code. Just uncomment it and use it. -->
+    <!-- <link href="css/style.css" rel="stylesheet" type="text/css"> -->
+
+    <!-- Revolution Slider 5.x CSS settings -->
+    <link
+      href="js/revolution-slider/css/settings.css"
+      rel="stylesheet"
+      type="text/css"
+    />
+    <link
+      href="js/revolution-slider/css/layers.css"
+      rel="stylesheet"
+      type="text/css"
+    />
+    <link
+      href="js/revolution-slider/css/navigation.css"
+      rel="stylesheet"
+      type="text/css"
+    />
+
+    <!-- CSS | Theme Color -->
+    <link
+      href="css/colors/theme-skin-orange.css"
+      rel="stylesheet"
+      type="text/css"
+    />
+
+    <!-- external javascripts -->
+    <script src="js/jquery-2.2.4.min.js"></script>
+    <script src="js/jquery-ui.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- JS | jquery plugin collection for this theme -->
+    <script src="js/jquery-plugin-collection.js"></script>
+
+    <!-- Revolution Slider 5.x SCRIPTS -->
+    <script src="js/revolution-slider/js/jquery.themepunch.tools.min.js"></script>
+    <script src="js/revolution-slider/js/jquery.themepunch.revolution.min.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body class="">
+    <div id="wrapper" class="clearfix">
+      <!-- preloader -->
+      <div id="preloader">
+        <div id="spinner">
+          <img
+            class="floating"
+            src="images/dheera/favicon.png"
+            alt="DHEERA Foundation"
+          />
+          <h5 class="line-height-50 font-18 ml-15">Loading...</h5>
+        </div>
+        <div id="disable-preloader" class="btn btn-default btn-sm">
+          Disable Preloader
+        </div>
+      </div>
+
+      <!-- Header -->
+      <header class="header">
+        <div class="header-nav">
+          <div class="header-nav-wrapper navbar-scrolltofixed bg-light">
+            <div class="container">
+              <div class="row navi-bar">
+                <div class="col-xs-6 col-lg-6">
+                  <a
+                    class="menuzord-brand pull-left flip xs-pull-center mb-15"
+                    href="index.html"
+                    ><img
+                      src="images/dheera/dheera-logo.png"
+                      alt="Dheera Foundation"
+                    />
+                  </a>
+                </div>
+                <div class="col-xs-6 col-lg-6">
+                  <nav id="menuzord" class="menuzord bg-light styl_nav">
+                    <ul class="menuzord-menu onepage-nav stly_ulnav">
+                      <li class="current active"><a href="#home">Home</a></li>
+                      <li class=""><a href="#about">About</a></li>
+                      <li class=""><a href="#causes">Our Works</a></li>
+                      <li><a href="#mission">Campaigns</a></li>
+                      <!-- <li><a href="#volunteers">Volunteers</a></li> -->
+                      <li><a href="#gallery">Media Center</a></li>
+                      <!-- <li><a href="#blog">Events</a></li> -->
+                      <li><a href="#contact">Contact Us</a></li>
+                      <li class="scrollable-fix"></li>
+                    </ul>
+                  </nav>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
